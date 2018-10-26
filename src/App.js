c24a271fb5970e3a7e8096cf314155412250f108
Mike Dobrowolski <mdobro@umich.edu>
2018-10-26 12:45:05 -0400

    flips master and detail views

--- src/App.js
+++ src/App.js
@@ -7,28 +7,12 @@ class App extends Component {
     return (
       <div>
         <div className="Master">
-          
+
         </div>
         <div className="Detail">
 
         </div>
       </div>
-      // <div className="App">
-      //   <header className="App-header">
-      //     <img src={logo} className="App-logo" alt="logo" />
-      //     <p>
-      //       Edit <code>src/App.js</code> and save to reload.
-      //     </p>
-      //     <a
-      //       className="App-link"
-      //       href="https://reactjs.org"
-      //       target="_blank"
-      //       rel="noopener noreferrer"
-      //     >
-      //       Learn React
-      //     </a>
-      //   </header>
-      // </div>
     );
   }
 }
